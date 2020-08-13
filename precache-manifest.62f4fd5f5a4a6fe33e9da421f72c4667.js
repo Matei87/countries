e2907416390bbb116f950fac019d7810edbeb685
Matei Mircea <mrc_matei@yahoo.com>
2020-08-13 18:00:39 +0300

    Updates

---
+++ precache-manifest.62f4fd5f5a4a6fe33e9da421f72c4667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84b4c3db100f9858c7bb0d31961f573",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "2f1513dfc4fb957d63e6",
+    "url": "/countries/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fd85a80c16df79b7e7d2",
+    "url": "/countries/static/css/main.c32f7f0c.chunk.css"
+  },
+  {
+    "revision": "2f1513dfc4fb957d63e6",
+    "url": "/countries/static/js/2.151b1261.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countries/static/js/2.151b1261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd85a80c16df79b7e7d2",
+    "url": "/countries/static/js/main.f3263b4d.chunk.js"
+  },
+  {
+    "revision": "77f7fa417b436e0f3333",
+    "url": "/countries/static/js/runtime-main.3a6eceb3.js"
+  }
+]);

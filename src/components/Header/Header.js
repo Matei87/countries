f2a1fe04cb4e7d4e75a69e553bfe3d18c5501e76
Matei Mircea <mrc_matei@yahoo.com>
2020-09-05 00:43:18 +0300

    global theme update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ const Header = (props) => {
             <div className="container-fluid">
                 <h2>Where in the world?</h2>
                 <button className="btn btn-outline-primary" onClick={props.toggleTheme} >{props.theme === 'light' ? <>
-                    <i className="fas fa-moon" /> <span>Dark</span></> : <>
-                        <i className="fas fa-sun" style={{ color: 'gold' }} /> <span style={{ color: 'gold' }}>Light</span></>}</button>
+                    <i className="fas fa-moon" /> </> : <>
+                        <i className="fas fa-sun" style={{ color: 'gold' }} /> </>}</button>
             </div>
         </header>
     )
